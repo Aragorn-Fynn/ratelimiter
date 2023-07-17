4f0ea5d1b0435f17b4e283fc9cd7457aecd244d2
FENG <553038170@qq.com>
2023-07-17 16:44:05 +0800

    init

---
+++ src/main/java/ratelimiter/App.java
@@ -0,0 +1,13 @@
+package ratelimiter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
